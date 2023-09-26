789e2780d2f7905280ba4f82d071121f5a035e09
Naoki051 <henriquenaoki051@gmail.com>
2023-09-26 10:52:08 -0300

    alterando o arquivo para mandar emails

--- linkFrontBack/my-node-app/src/routes/email.js
+++ linkFrontBack/my-node-app/src/routes/email.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const routes = express.Router();
+const sgMail = require("@sendgrid/mail");
+
+require("dotenv").config();
+sgMail.setApiKey(process.env.SG_KEY);
 
 routes.post("/envia-email", (req, res, next) => {
   const { nome, email, assunto, mensagem } = req.body;
+  const msg = {
+    to: process.env.TO_MAIL,
+    from: email,
+    subject: assunto,
+    text: `${nome}: ${mensagem}`,
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email enviado com sucesso!");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   return res.status(201).json({
     nome: nome,
     email: email,
